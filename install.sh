83fdcc9967c12240216323ecf1fe8c42aa1fc416
KadomiShikaya <46060085+KadomiShikaya@users.noreply.github.com>
2021-04-18 01:21:25 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+mkdir mywp
+cd mywp
+
+sudo apt-get update
+sudo apt install yum
+
+curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+
+sudo service docker start
+
+systemctl enable docker.service
+
+sudo usermod -a -G docker ec2-user
+
+curl -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+curl https://raw.githubusercontent.com/KadomiShikaya/wordpress/main/docker-compose.yaml?token=AK7NENJFBZCCGKRVZVNBCGTAPJWPE -o docker-compose.yaml
+
+docker-compose pull
+docker-compose up -d
